7c486856f7b97083f128d019b35ef1a31e030343
tkms0106 <23391543+tkms0106@users.noreply.github.com>
2018-06-26 13:36:34 +0900

    [clean]Separation of Concerns 3

--- vendingMachine.js
+++ vendingMachine.js
@@ -3,24 +3,7 @@ var balanceManager = require('./balanceManager');
 var changeHandler = require('./changeHandler');
 var productInventory = require('./productInventory');
 
-var products = [
-  {
-    name: 'Skittles',
-    price: 85,
-    id: 'A1'
-  }
-];
-
 module.exports = {
-  getProducts: function() {
-    return products;
-  },
-
-  getProduct: function(productId) {
-    var product = products.find(function(p) { return p.id === productId; });
-    return product;
-  },
-
   insertCoin: function(coinType){
     var value = changeHandler.getAmount(coinType);
     balanceManager.increaseBalance(value);
@@ -41,7 +24,7 @@ module.exports = {
   },
 
   vendProduct: function(productId){
-    var product = this.getProduct(productId);
+    var product = productInventory.getProduct(productId);
     balanceManager.decreaseBalance(product.price);
     return product;
   }
